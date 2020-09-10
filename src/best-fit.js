17a86cb41910b1168bf9429cc38411a94260ef05
Amit <amitgupta.gwl@gmail.com>
2020-09-10 12:46:18 +0530

    Distribute and balance scenarios among multiple child processes

---
+++ src/best-fit.js
@@ -0,0 +1,105 @@
+
+function flat(features, groups, scenarios){
+    for(let f_i=0; f_i < features.length; f_i++){
+        const feature = features[f_i];
+        const id = "f"+f_i;
+        const scenarioCount = feature.stats.total - feature.stats.skipped;
+        scenarios[id] = scenarioCount;
+        if(scenarioCount === 0) continue;
+
+        groups.push({
+            id: f_i,
+            fileName: feature.fileName,
+            features: [f_i],
+            scenarios: scenarioCount,
+        });
+    }
+}
+
+function compare(f1,f2){
+    if(f1.scenarios > f2.scenarios) return 1;
+    else if(f1.scenarios < f2.scenarios) return -1;
+    return 0;
+}
+
+function bestFit(features, binCount, threshold){
+    threshold = threshold || 5;
+    const groups = [];
+    const scenarios = {};
+    flat(features, groups, scenarios);
+    groups.sort(compare);
+    const totalScenarios = sum(groups);
+    let avg = Math.ceil(totalScenarios / binCount);
+    if(totalScenarios < threshold){
+        const featureGroups = [{
+            indexes: [],
+            scenarios: 0
+        }];
+        for (let i = 0; i < groups.length; i++) {
+            featureGroups[0].indexes.push(i);
+            featureGroups[0].scenarios += groups[i].scenarios
+        }
+        return featureGroups;
+    }else if(avg < binCount){
+        avg = threshold;
+        binCount = Math.floor(totalScenarios/avg);
+    }else if(groups.length < binCount){
+        const featureGroups = [];
+        for (let i = 0; i < groups.length; i++) {
+            featureGroups.push({
+                indexes: groups[i].features,
+                scenarios: groups[i].scenarios
+            });
+        }
+        return featureGroups;
+    }
+
+    const bin_boundary = groups.length - binCount - 1;
+    const featureGroups = [];
+    for (let i = bin_boundary + 1; i < groups.length; i++) {
+        featureGroups.push({
+            indexes: groups[i].features,
+            scenarios: groups[i].scenarios
+        });
+    }
+    for(let i=bin_boundary; i> -1; i--){
+        let fitToABin = false;
+        for(let bin_i=featureGroups.length - 1; bin_i > -1; bin_i--){
+            const newCount = featureGroups[bin_i].scenarios + groups[i].scenarios;
+            if( newCount <= avg ) {
+                fitToABin = true;
+                featureGroups[bin_i].scenarios = newCount;
+                featureGroups[bin_i].indexes.push(groups[i].id);
+                break;
+            }
+        }
+        if(!fitToABin){
+            const bin = findBinWithLessScenarios(featureGroups)
+            bin.indexes.push(groups[i].id);
+            bin.scenarios += groups[i].scenarios;
+        }
+    }
+    if(totalScenarios !== sum(featureGroups)){
+        console.warn("Unable to distribute scenarios in "+ binCount+ " processes.");
+    }
+    return featureGroups;
+}
+
+function findBinWithLessScenarios(featureGroups, featureToFit){
+    let lowest = Number.POSITIVE_INFINITY;
+    let selectedFeature;
+    for (let i=featureGroups.length-1; i> -1; i--) {
+        if (featureGroups[i].scenarios < lowest) {
+            selectedFeature = featureGroups[i];
+            lowest = featureGroups[i].scenarios;
+        }
+    }
+    return selectedFeature;
+}
+function sum(groups){
+    let s = groups[0].scenarios;
+    for(let i=1; i< groups.length; i++) s+= groups[i].scenarios;
+    return s;
+}
+
+module.exports = bestFit;
---
+++ test/best-fit_test.js
@@ -0,0 +1,86 @@
+const bestFit = require("./../src/best-fit");
+
+describe("BestFit", () => {
+    it("should resolve ", () => {
+        const features = buildTestData([3,7,17,23,8,5,6,14,21,15,8,13,14,17,8,9,7,12,14]);
+        const output = bestFit(features, 4);
+        const expected = [
+            { indexes: [ 2, 14, 10, 4, 1, 6 ], scenarios: 54 },
+            { indexes: [ 13, 11, 17, 15, 5 ], scenarios: 56 },
+            { indexes: [ 8, 12, 7, 16 ], scenarios: 56 },
+            { indexes: [ 3, 9, 18, 0 ], scenarios: 55 }
+          ];
+        expect(output).toEqual(expected);
+    });
+
+    it("when there are some 0 scenarios features", () => {
+        const features = buildTestData([3,7,0,23,0,5,6,0,21,0,8,13,14,17,8,9,7,12,14]);
+        const output = bestFit(features, 4);
+        const expected = [
+            { indexes: [ 18, 10, 16, 1, 6 ], scenarios: 42 },
+            { indexes: [ 13, 17, 15, 0 ], scenarios: 41 },
+            { indexes: [ 8, 11, 14 ], scenarios: 42 },
+            { indexes: [ 3, 12, 5 ], scenarios: 42 }
+          ];
+        expect(output).toEqual(expected);
+    });
+
+    it("when limit is higher than total scenarios ", () => {
+        const features = buildTestData([3,2]);
+        const output = bestFit(features, 4);
+        const expected = [ { indexes: [ 0, 1 ], scenarios: 5 } ];
+        //console.log(output);
+        expect(output).toEqual(expected);
+    });
+    it("when limit is higher than total scenarios ", () => {
+        const features = buildTestData([30,20]);
+        const output = bestFit(features, 4);
+        const expected =  [
+            { indexes: [ 1 ], scenarios: 20 },
+            { indexes: [ 0 ], scenarios: 30 }
+          ];
+        expect(output).toEqual(expected);
+    });
+    it("when scenarios are very less in a bin", () => {
+        const features = buildTestData([3,2, 1, 1,1,1,1,1,1]);
+        const output = bestFit(features, 4, 5);
+        const expected = [
+            { indexes: [ 1, 6, 5, 4, 2 ], scenarios: 6 },
+            { indexes: [ 0, 8, 7, 3 ], scenarios: 6 }
+          ];
+        expect(output).toEqual(expected);
+    });
+    it("when scenarios count is high", () => {
+        const features = buildTestData([56,57,54,51,59,52,54]);
+        const output = bestFit(features, 4);
+        const expected = [
+            { indexes: [ 6, 2 ], scenarios: 108 },
+            { indexes: [ 0, 5 ], scenarios: 108 },
+            { indexes: [ 1, 3 ], scenarios: 108 },
+            { indexes: [ 4 ], scenarios: 59 }
+          ]
+        expect(output).toEqual(expected);
+    });
+    it("when scenarios count is very low", () => {
+        const features = buildTestData([3,1]);
+        const output = bestFit(features, 7);
+        const expected = [
+            { indexes: [ 0, 1 ], scenarios: 4 }
+        ];
+        expect(output).toEqual(expected);
+    });
+});
+
+function buildTestData(arr){
+    const features = [];
+    for(let i=0; i < arr.length; i++){
+        features.push({
+            fileName: "f"+i,
+            stats: {
+                total: arr[i],
+                skipped: 0
+            }
+        });
+    }
+    return features;
+}
