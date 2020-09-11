2d76f897ebec4a8da14bcaa230f43089b5043948
Amit <amitgupta.gwl@gmail.com>
2020-09-11 06:45:49 +0530

    Collect total & skipped scenarios when filtering a feature object

--- src/ScenarioFilter.js
+++ src/ScenarioFilter.js
@@ -3,51 +3,65 @@ const { forEachFeature, forEachScenarioIn, forEachRule} = require("./Repository"
 
 function filter(featureObj, runConfig){
     if(runConfig.tags){
-        featureObj = filterByTagExpression(featureObj, runConfig.tags);
+        filterByTagExpression(featureObj, runConfig.tags);
     }else if(runConfig.include){
-        featureObj = filterByPosition(featureObj, runConfig.include, true);
+        filterByPosition(featureObj, runConfig.include, true);
     }else if(runConfig.exclude){
-        featureObj = filterByPosition(featureObj, runConfig.exclude, false);
+        filterByPosition(featureObj, runConfig.exclude, false);
     }else{
-        featureObj = filterForPriorityTags(featureObj);
+        filterForPriorityTags(featureObj);
     }
-    return featureObj;
 }
 
 function filterByPosition(featureObj, arr, include){
-    let positionCounter = 1;
     forEachFeature(featureObj, feature => {
+        let positionCounter = 1;
+        const stats = {
+            skipped: 0,
+            total: 0
+        }
         forEachRule(feature, rule => {
             forEachScenarioIn( rule , scenario => {
                 if(include){
-                    if(arr.indexOf(positionCounter) === -1) scenario.skip= true;
+                    if(arr.indexOf(positionCounter) === -1) {
+                        scenario.skip= true;
+                        stats.skipped +=1;
+                    }
                 }else{
-                    if(arr.indexOf(positionCounter) !== -1) scenario.skip= true; 
+                    if(arr.indexOf(positionCounter) !== -1) {
+                        scenario.skip= true; 
+                        stats.skipped +=1;
+                    }
                 }
                 positionCounter++;
             })
         })
+        stats.total = positionCounter - 1;
+        feature.stats = stats;
     });
-
-    return featureObj;
+    
 }
 
 function filterByTagExpression(featureObj, tagExpression){
-    
     const tagExpResolver = new BexpParser("("+tagExpression+") but not @skip");
 
     forEachFeature(featureObj, feature => {
+        const stats = {
+            skipped: 0,
+            total: 0
+        }
         forEachRule(feature, rule => {
             forEachScenarioIn( rule , scenario => {
                 const shouldRun = tagExpResolver.test(feature.tags.concat(scenario.tags));
                 if(!shouldRun){
-                    scenario.skip
+                    scenario.skip = true;
+                    stats.skipped++;
                 }
+                stats.total++;
             })
         })
+        feature.stats = stats;
     });
-    
-    return featureObj;
 }
 
 /**
@@ -55,33 +69,41 @@ function filterByTagExpression(featureObj, tagExpression){
  * @param {object} featureObj 
  */
 function filterForPriorityTags(featureObj){
-    let hasOnlyTag = false;
+    
+    //Skip all the tags which are tagged as @skip
     forEachFeature(featureObj, feature => {
+        const stats = {
+            skipped: 0,
+            total: 0
+        }
+    
+        let hasOnlyTag = false;
         forEachRule(feature, rule => {
             forEachScenarioIn( rule , scenario => {
+                stats.total++;
                 const tags = feature.tags.concat(scenario.tags);
                 if(tags.indexOf("@skip") !== -1){
                     scenario.skip = true;
+                    stats.skipped++;
                 }else if(tags.indexOf("@only") !== -1){
                     hasOnlyTag = true;
                 }
             })
-        })
-    });
-
-    if(hasOnlyTag){
-        forEachFeature(featureObj, feature => {
+        });
+        if(hasOnlyTag){
+            stats.skipped = 0;
             forEachRule(feature, rule => {
                 forEachScenarioIn( rule , scenario => {
                     const tags = feature.tags.concat(scenario.tags);
                     if(tags.indexOf("@only") === -1){
                         scenario.skip = true;
+                        stats.skipped++;
                     }
                 })
             })
-        });  
-    }
-    return featureObj;
+        }
+        feature.stats = stats;
+    });
 }
 
 module.exports = filter;
