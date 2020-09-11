67bc31e1eaf53d4597604774434db7e5bff25a05
Amit <amitgupta.gwl@gmail.com>
2020-09-11 06:49:19 +0530

    Update ConfigBuilder
    1. No config file for cucumon runner
    2. Embeded ins processor
    3. support for external config to run multiple specs with different settings

--- src/ConfigBuilder.js
+++ src/ConfigBuilder.js
@@ -1,17 +1,11 @@
-///@ts-check
 const fs = require('fs');
 const path = require('path');
 
-const configFilePath = path.join( process.cwd(), "cucumon-runner-config.js");
-
 let config = {
-  specs:[],
   parser: {},
-  dist: false
+  dist: {}
 };
-if( fs.existsSync(configFilePath)){
-  config = Object.assign( config, require(configFilePath));
-}
+let filterConfig = {};
 
 //Read CLI arguments
   const args = process.argv;
@@ -21,22 +15,20 @@ if( fs.existsSync(configFilePath)){
   for (let i = 3; i < args.length; i++) {
     const arg = args[i];
     if(arg === "-s" || arg === "--spec"){
-      const val = args[++i];
-      config.specs.push({
-        files: val
-      })
-    }else if(arg === "--tag"){
-      setSpecFilter("tags",args[++i]);
+      filterConfig.specs = args[++i];
+    }else if(arg === "--tags"){
+      filterConfig.tags = args[++i];
     }else if(arg === "--include"){
-      setSpecFilter("include",args[++i]);
+      filterConfig.include = args[++i].split(",");
     }else if(arg === "--exclude"){
-      setSpecFilter("exclude",args[++i]);
+      filterConfig.exclude = args[++i].split(",");
+    }else if(arg === "--specs-config"){
+      filterConfig.specs = require( args[++i] );
     }else if(arg === "--parallel"){
       config.dist = false;
     }else if(arg === "--dist"){
       const val = args[++i];
-      if(val === "true" ){
-      }else{
+      if(val === "false" ){
         config.dist = false;
       }
     }else if(arg === "--dist-limit"){
@@ -52,29 +44,28 @@ if( fs.existsSync(configFilePath)){
     }
   }
 
-  if(config.dist){
-    const cpuCount = require('os').cpus().length;
-    if(!config.dist.limit){
-      config.dist.limit = cpuCount > 1 ? cpuCount - 1 : 1;
-    }else if(config.dist.limit >= cpuCount){
-      config.dist.limit = cpuCount;
-    }
-  }else{
-    config.dist.limit = 1;
+if(config.dist){
+  const cpuCount = require('os').cpus().length;
+  if(!config.dist.limit){
+    config.dist.limit = cpuCount > 1 ? cpuCount - 1 : 1;
+  }else if(config.dist.limit >= cpuCount){
+    config.dist.limit = cpuCount;
   }
+}else{
+  config.dist.limit = 1;
+}
 
-function setSpecFilter(key, val){
-  if(config.specs.length = 0){
-    config.specs.push({
-      [key]: val
-    })
-  }else{
-    config.specs[config.specs.length - 1][key] = val;
-  }
+if(!config.specs){
+  config.specs = [{
+    files: filterConfig.specs,
+    tags: filterConfig.tags,
+    include: filterConfig.include,
+    exclude: filterConfig.exclude,
+  }];
 }
 //End read CLI arguments
 
-//setup parser
+//setup for parsing feature files
 if (!fs.existsSync(".cucumon")){
   fs.mkdirSync(".cucumon");
 }else{
@@ -85,9 +76,6 @@ const Cucumon = require("cucumon");
 const featureFileParser =  new Cucumon({
   clubBgSteps : true,
 });
-if(config.parser.insProcessor){
-  config.parser.insProcessor(featureFileParser);
-}
 
 module.exports = {
   config: config,
