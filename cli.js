0d39e14f2774629da6d8fc3d290e0b52c6811bc0
Amit <amitgupta.gwl@gmail.com>
2020-09-25 06:17:17 +0530

    Add banner

--- cli.js
+++ cli.js
@@ -11,6 +11,30 @@ if(process.argv.indexOf("-h") !== -1 || process.argv.indexOf("--help") !== -1){
   process.exitCode = 1;
   //process.exit(1);
 }else{
+
+  console.log(`
+   .d8888b.           888                                      
+  d88P  Y88b          888                                      
+  888    888          888                                      
+  888        888  888 888888 .d88b.  888d888 888  888 .d8888b  
+  888        888  888 888   d88""88b 888P"   888  888 88K      
+  888    888 888  888 888   888  888 888     888  888 "Y8888b. 
+  Y88b  d88P Y88b 888 Y88b. Y88..88P 888     Y88b 888      X88 
+   "Y8888P"   "Y88888  "Y888 "Y88P"  888      "Y88888  88888P' 
+                  888                                          
+             Y8b d88P                                          
+              "Y88P"                                           
+  `);
+
+  /*console.log(`
+    Check cytorus-land for historical comparison, detail analysis, awesome 
+    reports, page flow and to integrate your test result with Kanban board, API documents etc.
+  `)*/
+
+  console.log(`
+  visit: amitkumargupta.work for more awesome opensource projects
+  `);
+
   let child_process = require('child_process');
   const fs = require('fs');
   const path = require('path');
